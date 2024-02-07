1cee31ba681a9eed1eb4baf8243444fdcf172640
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2024-02-07 15:48:35 -0300

    2 maneiras diferentes de fazer o maps com js

--- FrontEnd/Projetos/10.1 - Landingpage2 com js/src/js/function.js
+++ FrontEnd/Projetos/10.1 - Landingpage2 com js/src/js/function.js
@@ -1,43 +1,67 @@
 window.onload = function () {
-    var map;
-
-    async function initMap() {
-        //const { Map } = await google.maps.importLibrary("maps");
-        var mapProp = {
-            center: new google.maps.LatLng(-27.648598, -48.577423),
+	async function initMap1() {
+		//@ts-ignore
+        const { Map } = await google.maps.importLibrary("maps");
+		const cairo = { lat: 30.064742, lng: 31.249509 };
+		const map = new google.maps.Map(document.getElementById("mapa"), {
+			scaleControl: true,
+			center: cairo,
+			zoom: 12,
+		});
+		const infowindow = new google.maps.InfoWindow();
+		
+		var conteudo1 = '<p style="color:black;font-size:13px;">Endereço!</p>';
+		infowindow.setContent(conteudo1);
+		
+		const marker = new google.maps.Marker({ map, position: cairo });
+		
+		marker.addListener("click", () => {
+			infowindow.open(map, marker);
+		});
+	}
+	
+	// 2 metodo
+	
+	var map;
+    async function initMap2() {
+		var mapProp = {
+			center: new google.maps.LatLng(-27.648598, -48.577423),
             scrollwheel: false,
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
             //mapTypeId: google.maps.MapTypeId.SATELLITE
         }
-        map = new google.maps.Map(document.getElementById("mapa"), mapProp);
+        map = new google.maps.Map(document.getElementById("mapa2"), mapProp);
     }
-
+	
     function addMarker(lat,long,icon,content,click){
-        var latLng={'lat':lat,'lng':long};
-
+		var latLng={'lat':lat,'lng':long};
+		
         var marker = new google.maps.Marker({
-            position:latLng,
+			position:latLng,
             map:map,
             icon:icon
         });
-
+		
         var infoWindow = new google.maps.InfoWindow({
-            content: content,
+			content: content,
             maxWidth: 200,
             pixelOffset: new google.maps.Size(0,20)
         });
-
+		
         if(click == true){
-            google.maps.event.addListener(marker,'click',function(){
-                infoWindow.open(map,marker);
+			google.maps.event.addListener(marker,'click',function(){
+				infoWindow.open(map,marker);
             })
         }else{
-            infoWindow.open(map,marker);
+			infoWindow.open(map,marker);
         }
     }
-
-    initMap();
+	
+	initMap1();
+    initMap2();
     var conteudo = '<p style="color:black;font-size:13px;">Endereço!</p>';
     addMarker(-27.616637,-48.5923228,'',conteudo,true);
-}
+
+	
+};
