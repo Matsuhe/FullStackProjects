c8946cba5d668dfaff6b01d8952e9170d38844d3
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2024-02-06 23:32:02 -0300

    v2 com js

---
+++ FrontEnd/Projetos/10.1 - Landingpage2 com js/src/js/function.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+    var map;
+
+    async function initMap() {
+        //const { Map } = await google.maps.importLibrary("maps");
+        var mapProp = {
+            center: new google.maps.LatLng(-27.648598, -48.577423),
+            scrollwheel: false,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+            //mapTypeId: google.maps.MapTypeId.SATELLITE
+        }
+        map = new google.maps.Map(document.getElementById("mapa"), mapProp);
+    }
+
+    function addMarker(lat,long,icon,content){
+        var latLng={'lat':lat,'lng':long};
+
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map:map,
+            icon:icon
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 200,
+            pixelOffset: new google.maps.Size(0,20)
+        });
+
+        if(click == true){
+            google.maps.event.addListener(marker,'click',function(){
+                infoWindow.open(map,marker);
+            })
+        }else{
+            infoWindow.open(map,marker);
+        }
+    }
+
+    initMap();
+    var conteudo = '<p style="color:black;font-size:13px;">Endereço!</p>';
+    addMarker(-27.616637,-48.5923228,'',conteudo,true);
+}
