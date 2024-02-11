e7579159b147afd16ca1a8a3d5d4cbdf7473615a
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2024-02-10 22:40:09 -0300

    Primeira impressao no js

---
+++ FrontEnd/Aulas/Cap 10 - JQuery/src/js/functions.js
@@ -0,0 +1,21 @@
+// $(function() {
+//     alert( "The DOM is now loaded and can be manipulated." );
+// });
+
+// $(document).ready(function(){
+// 	alert("Olá mundo!: ready_function");
+// })
+
+// $(function(){
+// 	alert("Olá mundo! :Function");
+// })
+
+// espera todo o documento carregar(fotos,videos)
+$(window).on('load',function(){
+	alert("Olá mundo");
+})
+
+
+
+
+
