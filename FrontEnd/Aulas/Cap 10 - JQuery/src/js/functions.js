80b22d1f96694a7dfa65cdfa157c0f5d48dfc2a5
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2024-02-11 17:43:54 -0300

    treinando eventos em jsquery

--- FrontEnd/Aulas/Cap 10 - JQuery/src/js/functions.js
+++ FrontEnd/Aulas/Cap 10 - JQuery/src/js/functions.js
@@ -11,11 +11,43 @@
 // })
 
 // espera todo o documento carregar(fotos,videos)
-$(window).on('load',function(){
-	alert("Olá mundo");
-})
+// $(window).on('load',function(){
+// 	alert("Olá mundo");
+// })
 
+// $(function(){
 
+// 	$('.artigo1 > p').css('color','rgba(0, 0, 255, 0.6)');
+// 	console.log($('.artigo1 > p').css('color'));
 
+// });
 
+// === treinando eventos ===
 
+$(function () {
+	$(".artigo1").click(function () {
+		$(".artigo2").css("background-color", "red");
+	});
+	$(".artigo1").hover(
+		function () {
+			$(".artigo2").css("background-color", "blue");
+		},
+		function () {
+			$(".artigo2").css("background-color", "rgb(100,100,100)");
+		}
+	);
+
+	$("textarea")
+		.focus(function () {
+			//Executa alguma função quando existe foco no elemento.
+			console.log("Foco!");
+		})
+		.blur(function () {
+			//Executa uma função quando retiramos o foco.
+			console.log("Retiramos o foco!");
+		});
+
+	$("select").change(function () {
+		console.log("Meu select foi alterado!");
+	});
+});
