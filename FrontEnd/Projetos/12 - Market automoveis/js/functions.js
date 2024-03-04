ab0e3bce64bd6960f4ef46e0aa790a50a5e36cf4
Matheus S. Volotão <matheus.volotao@hotmail.com>
2024-03-04 17:10:33 -0300

    Slider personalizado

--- FrontEnd/Projetos/12 - Market automoveis/js/functions.js
+++ FrontEnd/Projetos/12 - Market automoveis/js/functions.js
@@ -1,7 +1,7 @@
 $(function(){
 	
-		/*
-
+        /*
+          Sistema de pesquisa.
         */
 
         var currentValue = 0;
@@ -80,5 +80,90 @@ $(function(){
 	          $("body").css("-o-user-select","auto");
 	         $("body").css("user-select","auto");
         }
+
+        /*
+             Sistema de slide da página individual de cada carro.
+        */
+
+        var imgShow = 3;
+        var maxIndex = Math.ceil($('.mini-img-wraper').length/3) - 1;
+        var curIndex = 0;
+
+        initSlider();
+        navigateSlider();
+        clickSlider();
+        function initSlider(){
+           var amt = $('.mini-img-wraper').length * 33.3;
+           var elScroll = $('.nav-galeria-wraper');
+           var elSingle = $('.mini-img-wraper');
+           elScroll.css('width',amt+'%');
+           elSingle.css('width',33.3*(100/amt)+'%');
+        }
+
+        function navigateSlider(){
+             $('.arrow-right-nav').click(function(){
+                  if(curIndex < maxIndex){
+                      curIndex++;
+                      var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                      $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+                  }else{
+                     //console.log("Chegamos até o final!");
+                  }
+              });
+
+             $('.arrow-left-nav').click(function(){
+                 if(curIndex > 0){
+                      curIndex--;
+                      var elOff = $('.mini-img-wraper').eq(curIndex*3).offset().left - $('.nav-galeria-wraper').offset().left;
+                      $('.nav-galeria').animate({'scrollLeft':elOff+'px'});
+                  }else{
+                      //console.log("Chegamos até o final!");
+                  }
+             })
+        }
+
+
+        function clickSlider(){
+                $('.mini-img-wraper').click(function(){
+                   $('.mini-img-wraper').css('background-color','transparent');
+                   $(this).css('background-color','rgb(210,210,210)');
+                   var img = $(this).children().css('background-image');
+                   $('.foto-destaque').css('background-image',img);
+                })
+
+                $('.mini-img-wraper').eq(0).click();
+
+        }
+
+
+        /*
+          Clicar e ir para div de contato com base no atributo goto
+        */
+        var directory = '/Curso WebMaster/Projetos/projeto_05/'
+       
+        $('[goto=contato]').click(function(){
+            location.href=directory+'index.html?contato';
+            return false;
+        })
+        
+
+        checkUrl();
+
+
+        function checkUrl(){
+            var url = location.href.split('/');
+            var curPage = url[url.length-1].split('?');
+       
+            if(curPage[1] != undefined && curPage[1] == 'contato'){
+              $('header nav a').css('color','black');
+              $('footer nav a').css('color','white');
+              $('[goto=contato]').css('color','#EB2D2D');
+              $('html,body').animate({'scrollTop':$('#contato').offset().top});
+            }
+
+        }
+
+
+
 
 })
