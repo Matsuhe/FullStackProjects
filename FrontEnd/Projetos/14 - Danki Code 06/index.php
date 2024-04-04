1bdf9ce47e392df7bf888e81809151cb028d2bc9
Matheus S. Volotão <matheus.volotao@hotmail.com>
2024-04-04 18:26:53 -0300

    Renomeado nome de pastas e criando o projeto 06

---
+++ FrontEnd/Projetos/14 - Danki Code 06/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet">
+	<title>Projeto 06</title>
+</head>
+
+<body>
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+			<header>
+				<div class="icons-social">
+					<a href="https://facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				</div><!--icons-social-->
+				<div class="clear"></div><!-- clear -->
+				<div class="logo">
+					Danki.Code
+				</div><!--logo-->
+				<nav class="desktop">
+					<ul>
+						<li><a href="">Home</a></li>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+				<div class="clear"></div><!-- clear -->
+			</header>
+			<div class="conteudo-header">
+				<h2>Bem-vindo(a) ao nosso <span>Website</span></h2>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam sint, autem sit odio accusantium necessitatibus veniam id ipsam obcaecati dolorum molestiae blanditiis delectus. Excepturi cum doloribus harum reprehenderit vero?</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat laboriosam earum neque vel, perferendis consequuntur. Corporis, minima? Exercitationem, non facere. Est soluta obcaecati saepe consequuntur reprehenderit voluptate numquam quo culpa!</p>
+				<a href="#">Saiba mais</a>
+			</div><!-- conteudo-header -->
+		</div><!--center-->
+	</section><!--main-->
+	<section class="planos">
+		<div class="chamada-planos">
+			<div class="center">
+				<h2>Confira nossos planos</h2>
+			</div><!-- center -->
+		</div><!-- chamada-planos -->
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<div class="lore">
+							<h2>Lorem Ipsim</h2>
+						</div><!-- lore -->
+						<div class="pontos-plano">
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+						</div><!-- pontos-plano -->
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!-- preco -->
+					</div><!-- box-planos-wraper -->
+				</div><!-- box-planos-single -->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<div class="lore">
+							<h2>Lorem Ipsim</h2>
+						</div><!-- lore -->
+						<div class="pontos-plano">
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+						</div><!-- pontos-plano -->
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!-- preco -->
+					</div><!-- box-planos-wraper -->
+				</div><!-- box-planos-single -->
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+						<div class="lore">
+							<h2>Lorem Ipsim</h2>
+						</div><!-- lore -->
+						<div class="pontos-plano">
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+							<p><i class="fa fa-check-square"></i>Lorem ipsum silor dormor</p>
+						</div><!-- pontos-plano -->
+						<div class="preco">
+							<h2>R$250,00</h2>
+						</div><!-- preco -->
+					</div><!-- box-planos-wraper -->
+				</div><!-- box-planos-single -->
+			</div><!-- box-planos-single -->
+			</div><!-- center -->
+		</section><!-- box-planos -->
+	</section><!-- planos -->
+</body>
+
+</html>
